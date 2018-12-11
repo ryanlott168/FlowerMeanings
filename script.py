c56eb0788501286e9cfab911fc93635a06928992
Ryan L <toranlyt@gmail.com>
2018-12-11 17:42:31 -0600

    App that uses Hash Map that uses separate chains to avoid collisions

---
+++ script.py
@@ -0,0 +1,42 @@
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
+
+class HashMap:
+  def __init__(self, array_size):
+    self.array_size = array_size
+    self.array = [LinkedList() for i in range(self.array_size)]
+    
+  def hash(self, key):
+    return sum(key.encode())
+  
+  def compress(self, hash_code):
+    return hash_code % self.array_size
+  
+  def assign(self, key, value):
+    hash_code = self.hash(key)
+    array_index = self.compress(hash_code)
+    
+    payload = Node([key,value])
+    list_at_array = self.array[array_index]
+    
+    for item in list_at_array:
+      if key == item[0]:
+        item[1] = value
+        return
+    list_at_array.insert(payload)
+    
+    
+  def retrieve(self, key):
+    hash_code = self.hash(key)
+    array_index = self.compress(hash_code)
+    list_at_index = self.array[array_index]
+    for item in list_at_index:
+      if key == item[0]:
+        return item[1]
+    return None
+  
+blossom = HashMap(len(flower_definitions))
+for flower in flower_definitions:
+  blossom.assign(flower[0],flower[1])
+  
+print(blossom.retrieve('daisy'))
